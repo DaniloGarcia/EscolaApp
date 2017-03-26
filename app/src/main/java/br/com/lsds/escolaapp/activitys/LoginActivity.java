5e0aaeab91172c5ab7ee1253c0ce34bc54d3eaae
DaniloGarcia <danilo.j.garcia@gmail.com>
2017-03-26 20:05:21 -0300

    Implementação da tela de alunos e menu lateral (em desenv)
    Implementação da tela de alunos e menu lateral (em desenv)

--- app/src/main/java/br/com/lsds/escolaapp/activitys/LoginActivity.java
+++ app/src/main/java/br/com/lsds/escolaapp/activitys/LoginActivity.java
@@ -1,16 +1,32 @@
 package br.com.lsds.escolaapp.activitys;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import br.com.lsds.escolaapp.R;
 import br.com.lsds.escolaapp.util.ActivityHomeAsUpEnabled;
 
 public class LoginActivity extends ActivityHomeAsUpEnabled {
 
+    Activity    activity;
+    Button      btnEntrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        activity = this;
+
+        btnEntrar = (Button) findViewById(R.id.btn_entrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, AlunosActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
