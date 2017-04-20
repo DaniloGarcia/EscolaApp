55b0bb99769e1957697a7270366092a77c1a6c98
DaniloGarcia <danilo.j.garcia@gmail.com>
2017-04-19 21:04:40 -0300

    Click na lista
    Click na lista

--- app/src/main/java/br/com/lsds/escolaapp/adapters/AlunosChamadosAdapter.java
+++ app/src/main/java/br/com/lsds/escolaapp/adapters/AlunosChamadosAdapter.java
@@ -2,10 +2,10 @@ package br.com.lsds.escolaapp.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -19,12 +19,19 @@ import br.com.lsds.escolaapp.models.Alunos;
 
 public class AlunosChamadosAdapter extends RecyclerView.Adapter<AlunosChamadosAdapter.AlunosViewHolder> {
 
+    public interface ListItemClickListener {
+        void onListItemClick(int clickedItemIndex);
+    }
+
     private Context context;
     private List<Alunos> alunos;
+    private ListItemClickListener mOnClickListener;
 
-    public AlunosChamadosAdapter(Context context, List<Alunos> alunos) {
+
+    public AlunosChamadosAdapter(Context context, List<Alunos> alunos, ListItemClickListener listener) {
         this.context = context;
         this.alunos = alunos;
+        this.mOnClickListener = listener;
     }
 
     @Override
@@ -38,6 +45,7 @@ public class AlunosChamadosAdapter extends RecyclerView.Adapter<AlunosChamadosAd
     public void onBindViewHolder(AlunosChamadosAdapter.AlunosViewHolder holder, int position) {
         Alunos aluno = alunos.get(position);
         holder.txtAluno.setText(aluno.getName());
+
     }
 
     @Override
@@ -45,11 +53,19 @@ public class AlunosChamadosAdapter extends RecyclerView.Adapter<AlunosChamadosAd
         return this.alunos != null ? this.alunos.size() : 0;
     }
 
-    public class AlunosViewHolder extends RecyclerView.ViewHolder {
+    public class AlunosViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView txtAluno;
-        public AlunosViewHolder(View itemView) {
+        public AlunosViewHolder(final View itemView) {
             super(itemView);
             txtAluno = (TextView) itemView.findViewById(R.id.tx_nome_aluno);
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            int clickedPosition = getAdapterPosition();
+            mOnClickListener.onListItemClick(clickedPosition);
+        }
+
     }
 }
